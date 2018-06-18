fe00c581cdfde51ca696abe687721a7330bb185c
David Tolnay <dtolnay@gmail.com>
2018-06-18 08:31:06 -0700

    Example of using tt_replace to write a closure macro

---
+++ examples/replace.rs
@@ -0,0 +1,55 @@
+//! This example demonstrates use of `tt_replace!` to implement a fanciful
+//! syntax for unary closures.
+
+#[macro_use]
+extern crate tt_call;
+
+/// Predicate that determines whether the input is the token `self`.
+/// <sup>**[tt-call]**</sup>
+///
+/// # Input
+///
+///   - `input = [{` any single token `}]`
+///
+/// # Output
+///
+///   - `is = [{` true or false `}]`
+macro_rules! is_lowercase_self {
+    {
+        $caller:tt
+        input = [{ self }]
+    } => {
+        tt_return! {
+            $caller
+            is = [{ true }]
+        }
+    };
+
+    {
+        $caller:tt
+        input = [{ $other:tt }]
+    } => {
+        tt_return! {
+            $caller
+            is = [{ false }]
+        }
+    };
+}
+
+/// Expands to a closure with one argument called `self`. For example
+/// `closure!(self + 1)` would expand to `|__value| __value + 1`.
+macro_rules! closure {
+    ($($expr:tt)+) => {
+        |__value| tt_call! {
+            macro = [{ tt_replace }]
+            condition = [{ is_lowercase_self }]
+            replace_with = [{ __value }]
+            input = [{ $($expr)+ }]
+        }
+    };
+}
+
+fn main() {
+    let add_one = closure!(self + 1);
+    println!("{}", add_one(1));
+}
