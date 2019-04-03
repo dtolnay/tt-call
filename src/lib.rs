e3cf396fb1f1af35abda43bc7c2f1c591c62139c
David Tolnay <dtolnay@gmail.com>
2019-04-03 10:31:16 -0700

    Release 1.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -333,7 +333,7 @@
 //! [`examples/comma_separated.rs`]: https://github.com/dtolnay/tt-call/blob/master/examples/comma_separated.rs
 //! [`parse_type!`]: macro.parse_type.html
 
-#![doc(html_root_url = "https://docs.rs/tt-call/1.0.4")]
+#![doc(html_root_url = "https://docs.rs/tt-call/1.0.5")]
 
 mod predicate;
 mod replace;
