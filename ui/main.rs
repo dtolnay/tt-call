08a30264f432c00d72c31555d42c05c1ea5d59cf
David Tolnay <dtolnay@gmail.com>
2019-05-04 15:04:04 -0700

    Switch ui tests to trybuild

--- ui/main.rs
+++ ui/main.rs
@@ -1,21 +1,5 @@
 #[test]
 fn ui() {
-    let mut config = compiletest::Config {
-        mode: compiletest::common::Mode::Ui,
-        src_base: std::path::PathBuf::from("cases"),
-        target_rustcflags: Some(String::from(
-            "\
-             --edition=2018 \
-             -Z unstable-options \
-             --extern tt_call \
-             ",
-        )),
-        build_base: std::path::PathBuf::from("target/ui"),
-        ..Default::default()
-    };
-
-    config.link_deps();
-    config.clean_rmeta();
-
-    compiletest::run_tests(&config);
+    let t = trybuild::TestCases::new();
+    t.compile_fail("cases/*.rs");
 }
